a134ff497d843b757a53770c74db0fbf98733051
Raheel Kotwal <2023.raheelk@isu.ac.in>
2024-01-29 15:48:16 +0530

    basic insertions

---
+++ arrays/insertion.cpp
@@ -0,0 +1,99 @@
+#include "iostream"
+#include "algorithm"
+using namespace std;
+
+void insertAtBeginning() {
+    int arr[100]={}, n;
+    cout<<"Enter number of values to add initially: ";
+    cin>>n;
+    int val=0,i;
+    for(i=0;i<n;i++) {
+        cout<<"Enter initial value"<<i<<": ";
+        cin>>val;
+        arr[i]=val;
+    }
+    cout<<"OG array: "; 
+    for(int j=0;j<n;j++){
+        cout<<arr[j]<<" ";
+    }
+    cout<<endl;
+    cout<<"Enter new data to add: ";
+    cin>>val;
+    while(i>=0) {
+        arr[i+1]=arr[i];
+        i--;
+    }
+    arr[0] = val;
+    cout<<i<<endl;
+    i=0;
+    cout<<"Array after append: ";
+    while(arr[i]!=0) {
+        cout<<arr[i]<<" ";
+        i++;
+    }
+    cout<<endl;
+}
+
+void insertValueAtEndForEndIsKnown() {
+    int arr[100], n;
+    fill_n(arr, 100, -1);
+    cout<<"Enter number of values to add initially: ";
+    cin>>n;
+    int val=0,i=0;
+    for(i=0;i<n;i++) {
+        cout<<"Enter initial value"<<i<<": ";
+        cin>>val;
+        arr[i]=val;
+    }
+    cout<<i;
+    cout<<"OG array: "; 
+    for(int j=0;j<n;j++){
+        cout<<arr[j]<<" ";
+    }
+    cout<<endl;
+    cout<<"Enter new data to add: ";
+    cin>>arr[i];
+    cout<<arr[i];
+    i=0;
+    while(arr[i]!=-1) {
+        cout<<arr[i];
+        i++;
+    }
+}
+
+void insertValueAtEndForEndIsUnknown() {
+    int arr[100], n;
+    fill_n(arr, 100, -1);
+    cout<<"Enter number of values to add initially: ";
+    cin>>n;
+    int val=0,i;
+    for(i=0;i<n;i++) {
+        cout<<"Enter initial value"<<i<<": ";
+        cin>>val;
+        arr[i]=val;
+    }
+    int count=0;
+    i=0;
+    while(arr[i]!=-1) {
+        count++;
+        i++;
+    }
+    cout<<"Array has "<<count<<" elements."<<endl;
+    cout<<"Enter value to add: ";
+    cin>>val;
+    arr[count+1]=val;
+    i=0;
+    cout<<"After inserting: "<<endl;
+    while(arr[i]==-1) {
+        cout<<arr[i];
+        i++;
+    }
+
+}
+
+int main() {
+    //insertAtBeginning();
+    //insertValueAtEndForEndIsKnown();
+    insertValueAtEndForEndIsUnknown();
+    return 0;
+}
