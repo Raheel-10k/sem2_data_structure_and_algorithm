9f19a57a2305aefd5568c8b0b436eb60f311e7a2
Raheel Kotwal <2023.raheelk@isu.ac.in>
2024-01-29 16:10:44 +0530

    insertion at given known position

--- arrays/insertion.cpp
+++ arrays/insertion.cpp
@@ -88,12 +88,51 @@ void insertValueAtEndForEndIsUnknown() {
         cout<<arr[i];
         i++;
     }
+}//where some data is already present and you are unaware of it.
 
+void insertAtGivenLocation() {
+    int arr[100], n;
+    fill_n(arr, 100, -1);
+    cout<<"Enter number of values to add initially: ";
+    cin>>n;
+    int val=0,i;
+    for(i=0;i<n;i++) {
+        cout<<"Enter initial value"<<i<<": ";
+        cin>>val;
+        arr[i]=val;
+    }
+    int count=0;
+    i=0;
+    while(arr[i]!=-1) {
+        count++;
+        i++;
+    }
+    int pos;
+    cout<<"Enter position where you want to enter new value: ";
+    cin>>pos;
+    if(arr[pos]==-1) {
+        cout<<"Enter value to add at this position: ";
+        cin>>arr[pos];
+    }
+    else{
+        for(i=count-1;i>=pos;i--) {
+            arr[i+1]=arr[i];
+        }
+        count++;//as one new value added is known.
+        cout<<"Enter value to add at this position: ";
+        cin>>arr[pos];
+    }
+
+    for(int i=0;i<100;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 
 int main() {
     //insertAtBeginning();
     //insertValueAtEndForEndIsKnown();
-    insertValueAtEndForEndIsUnknown();
+    //insertValueAtEndForEndIsUnknown();
+    insertAtGivenLocation();
     return 0;
 }
