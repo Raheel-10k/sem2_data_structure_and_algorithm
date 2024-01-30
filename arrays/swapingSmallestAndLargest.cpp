067b2a245838ff348d67b5c7586b3ed2f7ed14b3
Raheel Kotwal <2023.raheelk@isu.ac.in>
2024-01-30 14:54:55 +0530

    swapping largest and smallest of an array

---
+++ arrays/swapingSmallestAndLargest.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+using namespace std;
+
+void swappingLargestAndSmallest(int arr[], int n) {
+    int maxNumIndex=0, minNumIndex=0;
+    for(int i=0;i<n;i++) {
+        if(arr[maxNumIndex]<arr[i]) {
+            maxNumIndex=i;
+        }
+        if(arr[minNumIndex]>arr[i]) {
+            minNumIndex=i;
+        }
+    }
+    int temp=arr[maxNumIndex];
+    arr[maxNumIndex]=arr[minNumIndex];
+    arr[minNumIndex]=temp;
+    for(int i=0;i<n;i++) {
+        cout<<arr[i];
+    }
+}
+
+int main() {
+    int len;
+    cout<<"Enter the number of elements: ";
+    cin>>len;
+    int arr[len];
+    for(int i=0;i<len;i++) {
+        cin>>arr[i];
+    }
+    swappingLargestAndSmallest(arr, len);
+}
